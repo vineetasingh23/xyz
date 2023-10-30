3d393081b4d84facdf14c8aba198da992efdd01c
Vineeta Singh <65161314+vineeta23@users.noreply.github.com>
2023-10-31 04:17:04 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,19 +1,37 @@
-public class SqlQueryBuilder {
+import org.apache.poi.ss.usermodel.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-    public String buildUpdateObjectPropertiesQuery() {
-        return "UPDATE t_objectproperties SET value = :startTime " +
-                "WHERE property = 'lastUpdated' " +
-                "AND object_id = (SELECT object_id FROM t_object WHERE ea_guid = :parentGUID)";
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ExcelDataService {
+
+    public List<MyElement> readExcelData(MultipartFile file) throws IOException {
+        List<MyElement> elements = new ArrayList<>();
+
+        Workbook workbook = WorkbookFactory.create(file.getInputStream());
+        Sheet sheet = workbook.getSheetAt(0); // Assuming data is in the first sheet
+
+        int rows = sheet.getPhysicalNumberOfRows();
+
+        for (int i = 0; i < rows; i++) {
+            Row row = sheet.getRow(i);
+            if (row != null) {
+                MyElement myElement = new MyElement();
+
+                myElement.setGuid(row.getCell(0).getStringCellValue());
+                myElement.setCsvKey(row.getCell(1).getStringCellValue());
+                myElement.setCsvParentKey(row.getCell(2).getStringCellValue());
+                // Populate other properties...
+
+                elements.add(myElement);
+            }
+        }
+
+        workbook.close();
+        return elements;
     }
 }
-
-
- public void updateObjectProperties(String startTime, String parentGUID) {
-        String sql = sqlQueryBuilder.buildUpdateObjectPropertiesQuery();
-
-        MapSqlParameterSource params = new MapSqlParameterSource()
-                .addValue("startTime", startTime)
-                .addValue("parentGUID", parentGUID);
-
-        jdbcTemplate.update(sql, params);
-    }
