7fa562f12a4ab9591495236d5730277cb33d8d6f
Vineeta Singh <65161314+vineeta23@users.noreply.github.com>
2023-11-06 23:00:57 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,133 +1,29 @@
 @Service
-public class EAService {
+public class ElementService {
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private ElementRepository elementRepository;
 
-    public void setParentElement(List<MyElement> elements, String defaultParentPackage) {
-        for (MyElement element : elements) {
-            String parentKey = element.getCsvParentKey();
+    public Element getElementDetails(String objectID) {
+        Map<String, Object> result = elementRepository.getElements(objectID);
+        Element element = new Element();
 
-            if (parentKey != null && !parentKey.isEmpty()) {
-                if (parentKey.startsWith("{")) {
-                    try {
-                        MyElement parentElement = getElementByGUID(parentKey);
-                        
-                        if (parentElement != null && "Package".equals(parentElement.getType())) {
-                            setParentPackage(parentElement);
-                            element.setParentElementId(parentElement.getElementID());
-                        }
-                    } catch (Exception e) {
-                        // Handle errors when getting the element by GUID
-                    }
-                } else {
-                    MyElement csvParent = getCsvParentByKey(parentKey);
-                    if (csvParent == null) {
-                        setDefaultParentPackage(defaultParentPackage);
-                    } else {
-                        if ("Package".equals(csvParent.getType())) {
-                            element.setParentElementId(csvParent.getElementID());
-                            setParentPackage(csvParent);
-                        }
-                    }
-                }
-            }
-        }
-    }
+        // Mapping the retrieved values to the Element object
+        element.setObjectId((Integer) result.get("objectId"));
+        element.setName((String) result.get("elementName"));
+        element.setElementType((String) result.get("elementType"));
+        element.setGuid((String) result.get("guid"));
+        element.setAuthor((String) result.get("author"));
+        // ... map other attributes similarly
 
-    private MyElement getElementByGUID(String guid) {
-        // Your logic to fetch an element from the database by GUID
-        return null;
-    }
-
-    private MyElement getCsvParentByKey(String key) {
-        // Your logic to fetch the CSV parent element by key
-        return null;
-    }
-
-    private void setParentPackage(MyElement parentPackage) {
-        // Logic to handle setting the parent package
-    }
-
-    private void setDefaultParentPackage(String defaultParentPackage) {
-        // Logic to set a default parent package based on the user input
-    }
-}
-
-
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class ElementDAO {
-
-    private final JdbcTemplate jdbcTemplate;
-
-    public ElementDAO(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
-    public Element getElementByGUID(String elementGUID) {
-        String sql = "SELECT * FROM elements WHERE element_guid = ?";
-        return jdbcTemplate.queryForObject(sql, new Object[]{elementGUID}, new ElementMapper());
-    }
-
-    // ElementMapper is a RowMapper class to map query results to Element object
-    private static class ElementMapper implements RowMapper<Element> {
-        @Override
-        public Element mapRow(ResultSet rs, int rowNum) throws SQLException {
-            Element element = new Element();
-            element.setId(rs.getLong("id"));
-            element.setElementGUID(rs.getString("element_guid"));
-            // Set other properties accordingly
-            return element;
-        }
+        return element;
     }
 }
 
 
 
-String originalNotes = row.getCell(7).getStringCellValue(); // Replace with your data source
-String updatedNotes = originalNotes.replace("'", " ").replace("\"", " ");
-myElement.setNotes(updatedNotes);
-
-
-public void updateObjectProperties(String parentGUID, String starttime) {
-        String sql = "UPDATE t_objectproperties SET value = ? WHERE property = 'lastUpdated' " +
-                "AND object_id = (SELECT object_id FROM t_object WHERE ea_guid = ?)";
-        
-        jdbcTemplate.update(sql, starttime, parentGUID);
-
-
-    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"); // Adjust the format as needed
-LocalDateTime now = LocalDateTime.now();
-String startTime = now.format(formatter);
 
 
 
 
-    @Service
-public class DataExtractorDao {
-
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
-
-    public Elements getElementsForPackage(String packageID) {
-        String sql = "SELECT * FROM t_object WHERE package_id = ?";
-        
-        return jdbcTemplate.queryForObject(sql, new Object[]{packageID}, new ElementsRowMapper());
-    }
-}
-
-class ElementsRowMapper implements RowMapper<Elements> {
-    @Override
-    public Elements mapRow(ResultSet rs, int rowNum) throws SQLException {
-        Elements elements = new Elements();
-        elements.setId(rs.getInt("id"));
-        elements.setName(rs.getString("name"));
-        // Map other fields
-        
-        return elements;
-    }
-}
 
 
