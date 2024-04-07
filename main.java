0ea3864165d651f63017ca8cb18428488db3b297
Vineeta Singh <65161314+vineeta23@users.noreply.github.com>
2024-04-07 12:34:12 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,45 +1,48 @@
-import React, { useRef, useEffect } from 'react';
-import Quill from 'quill';
-import 'quill/dist/quill.snow.css';
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import './Editor.css';
 
-const Editor = () => {
-  const quillRef = useRef(null);
+function Editor() {
+  const [value, setValue] = useState('');
 
-  useEffect(() => {
-    if (!quillRef.current) return;
+  console.log(value);
 
-    const quill = new Quill(quillRef.current, {
-      theme: 'snow',
-      modules: {
-        toolbar: [
-          [{ header: [1, 2, false] }],
-          ['bold', 'italic', 'underline'],
-          [{ list: 'ordered' }, { list: 'bullet' }],
-          ['link', 'image'],
-        ],
-      },
-    });
+  const toolbarOptions = [
+    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+    ['bold', 'italic', 'underline', 'strike'],
+    ['blockquote', 'code-block'],
+    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+    [{ 'script': 'sub' }, { 'script': 'super' }],
+    [{ 'direction': 'rtl' }],
+    [{ 'size': ['small', false, 'large', 'huge'] }],
+    [{ 'color': [] }, { 'background': [] }],
+    [{ 'align': [] }],
+    ['link', 'image', 'video'],
+    ['clean'],
+  ];
 
-    quill.clipboard.addMatcher(Node.ELEMENT_NODE, (node, delta) => {
-      // Check if the node is an HTML table
-      if (node.tagName && node.tagName.toLowerCase() === 'table') {
-        const html = node.outerHTML;
-        delta.ops = [
-          {
-            insert: { table: html },
-          },
-        ];
-      }
+  const modules = {
+    toolbar: toolbarOptions,
+    clipboard: {
+      matchVisual: false, // Disables Quill's default HTML paste handler
+      matchers: [
+        [Node.ELEMENT_NODE, (node, delta) => {
+          if (node.tagName && node.tagName.toLowerCase() === 'table') {
+            const html = node.outerHTML;
+            return { ops: [{ insert: html }] };
+          }
+          return delta;
+        }],
+      ],
+    },
+  };
 
-      return delta;
-    });
-
-    return () => {
-      quill.off('text-change');
-    };
-  }, []);
-
-  return <div ref={quillRef} style={{ height: '400px' }} />;
-};
+  return (
+    <div className='Editor'>
+      <ReactQuill modules={modules} theme='snow' value={value} onChange={setValue} />
+    </div>
+  );
+}
 
 export default Editor;
