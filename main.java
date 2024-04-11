4b579d605981c6d1ccccf9c53acfff771f7ef95f
Vineeta Singh <65161314+vineeta23@users.noreply.github.com>
2024-04-11 14:46:43 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,48 +1,10 @@
-<div className="form-group" style={{ display: 'flex', justifyContent: 'center' }}>
-  {/* Upload Button */}
-  <Button
-    variant="contained"
-    sx={{
-      borderRadius: '10px',
-      textTransform: 'none',
-      marginBottom: 1,
-      marginTop: 1,
-      backgroundColor: 'rgba(183, 216, 255, 0.15)',
-      height: 25,
-      fontSize: '11px',
-      color: '#000000',
-      marginLeft: 'auto',
-      marginRight: 1,
-      boxShadow: 30,
-      ':hover': {
-        boxShadow: 30,
-        backgroundColor: 'rgba(183, 216, 255, 0.9)',
-      },
+style={{
+      border: 'none',
+      borderBottom: '1px solid #ccc', // Add a bottom border for the line effect
+      outline: 'none', // Remove outline on focus
+      background: 'transparent', // Make background transparent
+      width: '100%', // Optional: adjust width as needed
+      lineHeight: '1.5', // Optional: adjust line height for better appearance
+      fontSize: '14px', // Optional: adjust font size
+      padding: '4px 0', // Optional: adjust padding
     }}
-    onClick={handleUploadFiles}
-  >
-    <AttachmentOutlinedIcon sx={{ height: 20, width: 20, alignSelf: 'center' }} />
-    <span> Upload</span>
-  </Button>
-  <Button
-    variant="contained"
-    sx={{
-      borderRadius: '10px',
-      textTransform: 'none',
-      marginBottom: 1,
-      marginTop: 1,
-      backgroundColor: 'rgba(183, 216, 255, 0.15)',
-      height: 25,
-      fontSize: '11px',
-      color: '#000000',
-      ':hover': {
-        boxShadow: 30,
-        backgroundColor: 'rgba(183, 216, 255, 0.9)',
-      },
-    }}
-    onClick={handleRemoveFiles}
-  >
-    <ClearIcon sx={{ height: 20, width: 20, alignSelf: 'center', marginRight: 1 }} />
-    <span> Remove All</span>
-  </Button>
-</div>
