62fb62af77b82a97ae056e8258fc0efb39999a41
Vineeta Singh <65161314+vineeta23@users.noreply.github.com>
2024-06-17 20:22:47 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,57 +1,8 @@
-import React from "react";
-import AutorenewIcon from "@mui/icons-material/Autorenew";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import { styled } from "@mui/system";
-import { lightTheme, darkTheme } from "../Constants/theme";
-import clsx from "clsx";
-
-const RefreshIcon = styled(AutorenewIcon)(({ theme }) => ({
-  '&.spin': {
-    animation: 'spin 1s linear infinite',
-  },
-  '@keyframes spin': {
-    '0%': {
-      transform: 'rotate(0deg)',
-    },
-    '100%': {
-      transform: 'rotate(360deg)',
-    },
-  },
-}));
-
-const ColorButton = styled(Button)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'light' ? lightTheme.darkBlue : darkTheme.darkBlue,
-  position: 'absolute',
-  right: '1.4%',
-  borderRadius: '5px',
-  color: 'white',
-  '&:hover': {
-    backgroundColor: theme.palette.mode === 'light' ? lightTheme.darkBlue : darkTheme.darkBlue,
-    color: 'white',
-    fontWeight: 'bold',
-  },
-}));
-
-const RefreshButton = () => {
-  const [spin, setSpin] = React.useState(false);
-
-  const refresh = () => {
-    setSpin(true);
-    setTimeout(() => {
-      setSpin(false);
-      window.location.reload();
-    }, 1000);
+  const handleRemoveFiles = (index: number) => {
+    if (fileList) {
+      const filesArray = Array.from(fileList);
+      const updatedFileList = filesArray.filter((_, i) => i !== index);
+      setFileList(new DataTransfer().files);
+      updatedFileList.forEach(file => new DataTransfer().items.add(file));
+    }
   };
-
-  return (
-    <Box onClick={refresh}>
-      <ColorButton variant="contained" size="small">
-        <RefreshIcon className={clsx({ spin })} sx={{ mr: 0.5 }} />
-        Refresh
-      </ColorButton>
-    </Box>
-  );
-};
-
-export default RefreshButton;
