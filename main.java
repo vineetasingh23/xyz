274604b7434d3e004e1372687274b0762d69fd32
Vineeta Singh <65161314+vineeta23@users.noreply.github.com>
2024-06-20 12:17:02 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,2 +1,9 @@
- const queuePath = value.queueId === "0" ? 'queue/workflow' : `queue/${value.name}`;
- const fullPath = `/${queuePath}`;
+const formatQueuePath = (name) => {
+    return name.replace(/\s+/g, '').toLowerCase().replace(/-/g, '_');
+};
+
+ const queuePath = value.queueId === "0" ? 'queue/workflow' : `queue/${formatQueuePath(value.name)}`;
+                    const fullPath = `/${queuePath}`;
+
+backgroundColor: isActive(queuePath) ? 'sideBarClickColor' : 'inherit',
+                               
