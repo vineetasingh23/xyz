96e5ccef5bca14a7c8c190e947d0a8faa3089143
Vineeta Singh <65161314+vineeta23@users.noreply.github.com>
2024-06-28 13:13:55 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,18 +1,3 @@
- const getCreateRules = async () => {
-    try {
-      const url = ApiEndPoints.ROOT + ApiEndPoints.STATIC_RULES;
-      const axiosConfig: AxiosRequestConfig = {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Content-Type": "application/json",
-        },
-      };
-      const response = await ApiConfig.getCall(url, axiosConfig);
-      setRulesList(response.data);
-      setConditionParams(response.data.conditionParams);
-      setOperators(response.data.operators);
-      console.log(response);
-    } catch (error) {
-      console.error("Error fetching create rules table:", error);
-    }
-  };
+ {conditionParams.map((value) => (
+                <MenuItem key={value.value} value={value.value}>{value.value}</MenuItem>
+              ))}
