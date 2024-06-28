fd5b3cd4ff11c19a9a3bad86d4614ff51e902761
Vineeta Singh <65161314+vineeta23@users.noreply.github.com>
2024-06-28 13:09:41 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,16 +1,18 @@
- const getRulesList = async () => {
+ const getCreateRules = async () => {
     try {
-      const url = ApiEndPoints.ROOT + ApiEndPoints.VIEW_RULES;
+      const url = ApiEndPoints.ROOT + ApiEndPoints.STATIC_RULES;
       const axiosConfig: AxiosRequestConfig = {
         headers: {
           "Access-Control-Allow-Origin": "*",
           "Content-Type": "application/json",
-        }
+        },
       };
       const response = await ApiConfig.getCall(url, axiosConfig);
-      setRulesList(response.data.items);  // Assuming the response data structure
+      setRulesList(response.data);
+      setConditionParams(response.data.conditionParams);
+      setOperators(response.data.operators);
       console.log(response);
     } catch (error) {
-      console.error("Error fetching rules list:", error);
+      console.error("Error fetching create rules table:", error);
     }
   };
