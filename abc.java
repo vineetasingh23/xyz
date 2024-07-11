dfd73ff3af3cf03d3b3d117b81c83e8a92d56f37
Vineeta Singh <65161314+vineeta23@users.noreply.github.com>
2024-07-11 13:42:41 +0530

    Update abc.java

--- abc.java
+++ abc.java
@@ -9,6 +9,42 @@ import Collapse from '@mui/material/Collapse';
 import Divider from '@mui/material/Divider';
 import { Box } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { styled } from '@mui/system';
+
+const ScrollbarStyledList = styled(List)(({ theme }) => ({
+  overflowY: 'auto',
+  overflowX: 'hidden',
+  flexGrow: 1,
+  '&::-webkit-scrollbar': {
+    width: '12px',
+  },
+  '&::-webkit-scrollbar-thumb': {
+    backgroundColor: '#888', // Thumb color
+    borderRadius: '10px',
+  },
+  '&::-webkit-scrollbar-thumb:hover': {
+    backgroundColor: '#555', // Thumb hover color
+  },
+  '&::-webkit-scrollbar-track': {
+    backgroundColor: '#f1f1f1', // Track color
+    borderRadius: '10px',
+  },
+  '&::-ms-overflow-style': 'auto', // For Edge
+  '&::-ms-scrollbar': {
+    width: '12px',
+  },
+  '&::-ms-scrollbar-thumb': {
+    backgroundColor: '#888', // Thumb color
+    borderRadius: '10px',
+  },
+  '&::-ms-scrollbar-thumb:hover': {
+    backgroundColor: '#555', // Thumb hover color
+  },
+  '&::-ms-scrollbar-track': {
+    backgroundColor: '#f1f1f1', // Track color
+    borderRadius: '10px',
+  },
+}));
 
 function Sidebar({ open, queues, queuesOpen }) {
   return (
@@ -18,76 +54,68 @@ function Sidebar({ open, queues, queuesOpen }) {
       id="drawer"
       PaperProps={{
         sx: {
-          height: "100vh",
-          backgroundColor: "headerandSidebarColor",
-          display: "flex",
-          overflow: "hidden",
+          height: '100%',
+          backgroundColor: 'headerandSidebarColor',
+          display: 'flex',
+          flexDirection: 'column',
         },
       }}
     >
       <Toolbar variant="dense" />
-      <List>
-        <ListItem disablePadding sx={{ display: "block" }}>
+      <List sx={{ flexGrow: 1 }}>
+        <ListItem disablePadding sx={{ display: 'block' }}>
           <Tooltip title="Template" placement="right">
             <ListItemButton>...</ListItemButton>
           </Tooltip>
         </ListItem>
-        <ListItem disablePadding sx={{ display: "block" }}>
+        <ListItem disablePadding sx={{ display: 'block' }}>
           <Tooltip title="Outbox" placement="right">
             <ListItemButton>...</ListItemButton>
           </Tooltip>
         </ListItem>
-        <ListItem disablePadding sx={{ display: "block" }}>
+        <ListItem disablePadding sx={{ display: 'block' }}>
           <Tooltip title="Sent Items" placement="right">
             <ListItemButton>...</ListItemButton>
           </Tooltip>
         </ListItem>
-        <ListItem disablePadding sx={{ display: "block" }}>
+        <ListItem disablePadding sx={{ display: 'block' }}>
           <Tooltip title="Common Bin" placement="right">
             <ListItemButton>...</ListItemButton>
           </Tooltip>
         </ListItem>
-        <ListItem disablePadding sx={{ display: "block" }}>
+        <ListItem disablePadding sx={{ display: 'block' }}>
           <Tooltip title="Linked Queue" placement="right">
             <ListItemButton>...</ListItemButton>
           </Tooltip>
         </ListItem>
-        <ListItem disablePadding sx={{ display: "block" }}>
-          <Tooltip title={"Queues Count:" + (queues?.length || 0)} placement="right">
+        <ListItem disablePadding sx={{ display: 'block' }}>
+          <Tooltip title={'Queues Count:' + (queues?.length || 0)} placement="right">
             <ListItemButton>...</ListItemButton>
           </Tooltip>
         </ListItem>
         <Collapse in={queuesOpen} timeout="auto" unmountOnExit>
           <Divider />
-          <List
-            sx={{
-              mr: "15px",
-              overflowY: "auto",
-              overflowX: "hidden",
-              flexGrow: 1,
-            }}
-            component={Box}
-            disablePadding
-          >
-            {queues && queues.map((value, index) => (
-              <ListItemButton
-                key={index}
-                component={Link}
-                to={`/queue/${value.id}`}
-                sx={{
-                  minHeight: 48,
-                  px: 2.5,
-                  ml: open && index !== 0 ? 2.6 : 'auto',
-                  backgroundColor: isActive(value.name) ? "sideBarClickColor" : "inherit",
-                  "&:hover": {
-                    backgroundColor: isActive(value.name) ? "sideBarClickColor" : "inherit",
-                  },
-                }}
-              >
-                {value.name}
-              </ListItemButton>
-            ))}
-          </List>
+          <ScrollbarStyledList component={Box} disablePadding>
+            {queues &&
+              queues.map((value, index) => (
+                <ListItemButton
+                  key={index}
+                  component={Link}
+                  to={`/queue/${value.id}`}
+                  sx={{
+                    minHeight: 48,
+                    px: 2.5,
+                    ml: open && index !== 0 ? 2.6 : 'auto',
+                    backgroundColor: isActive(value.name) ? 'sideBarClickColor' : 'inherit',
+                    '&:hover': {
+                      backgroundColor: isActive(value.name) ? 'sideBarClickColor' : 'inherit',
+                    },
+                  }}
+                >
+                  {value.name}
+                </ListItemButton>
+              ))}
+          </ScrollbarStyledList>
         </Collapse>
       </List>
     </Drawer>
