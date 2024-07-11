3dbbba0cdb42f283f6eaa3cb3f4746b400509b51
Vineeta Singh <65161314+vineeta23@users.noreply.github.com>
2024-07-11 11:23:34 +0530

    Update abc.java

--- abc.java
+++ abc.java
@@ -1,26 +1,19 @@
-import React from 'react';
-import TextField from '@mui/material/TextField';
-import { styled } from '@mui/system';
-
 const CustomTextField = styled(TextField)(({ theme }) => ({
-  '& .MuiInputBase-input': {
-    color: 'red', // Change text color
-    fontFamily: 'Arial', // Change font family
-    fontSize: '16px', // Change font size
+  '& .MuiOutlinedInput-root': {
+    width: '100%',
+    '& fieldset': {
+      borderColor: theme.palette.primary.main,
+    },
+    '&:hover fieldset': {
+      borderColor: theme.palette.secondary.main,
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: theme.palette.secondary.main,
+    },
+    '& .MuiInputBase-input': {
+      color: 'red', // Change text color
+      fontFamily: 'Arial', // Change font family
+      fontSize: '16px', // Change font size
+    },
   },
-  width: '100%', // Set the width to 100%
 }));
-
-function App() {
-  return (
-    <CustomTextField
-      variant="outlined"
-      disabled
-      value="Your text here"
-      multiline
-      rows={2}
-    />
-  );
-}
-
-export default App;
