a31f3d765e4ad8d9eaf0cef9d5eb81199c3b94af
Vineeta Singh <65161314+vineeta23@users.noreply.github.com>
2024-07-12 11:18:55 +0530

    Update abc.java

--- abc.java
+++ abc.java
@@ -1,83 +1,48 @@
-import React, { useState } from 'react';
-import Drawer from '@mui/material/Drawer';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import Tooltip from '@mui/material/Tooltip';
-import Collapse from '@mui/material/Collapse';
-import Divider from '@mui/material/Divider';
-import { Box, TextField } from '@mui/material';
-import { Link } from 'react-router-dom';
 
-function Sidebar({ open, queues, queuesOpen }) {
-  const [searchInput, setSearchInput] = useState('');
 
-  const handleSearchChange = (event) => {
-    setSearchInput(event.target.value);
-  };
+          {queues?.length > 7 && (
+          <ListItem disablePadding sx={{ display: "block" }}>
+            <TextField
+              variant="outlined"
+              placeholder="Search Queues"
+              value={searchInput}
+              onChange={handleSearchChange}
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon style={{ color: 'white' }} />
+                  </InputAdornment>
+                ),
+                style: { color: 'white' },
+              }}
+              sx={{
+                '& .MuiOutlinedInput-root': {
+                  '& fieldset': {
+                    borderColor: 'white',
+                  },
+                  '&:hover fieldset': {
+                    borderColor: 'white',
+                  },
+                  '&.Mui-focused fieldset': {
+                    borderColor: 'white',
+                  },
+                },
+                '& .MuiInputLabel-root': {
+                  color: 'white',
+                },
+                '& .MuiInputLabel-root.Mui-focused': {
+                  color: 'white',
+                },
+              }}
+            />
+          </ListItem>
+        )}
 
-  const filteredQueues = queues?.filter((queue) =>
-    queue.name.toLowerCase().includes(searchInput.toLowerCase())
-  );
 
-  return (
-    <Drawer
-      variant="permanent"
-      open={open}
-      id="drawer"
-      PaperProps={{
-        sx: {
-          height: "100%",
-          backgroundColor: "headerandSidebarColor",
-          display: "flex",
-          flexDirection: "column",
-        },
-      }}
-    >
-      <Toolbar variant="dense" />
-      <List sx={{ flexGrow: 1 }}>
-        <ListItem disablePadding sx={{ display: "block" }}>
-          <Tooltip title="Template" placement="right">
-            <ListItemButton>...</ListItemButton>
-          </Tooltip>
-        </ListItem>
-        <ListItem disablePadding sx={{ display: "block" }}>
-          <Tooltip title="Outbox" placement="right">
-            <ListItemButton>...</ListItemButton>
-          </Tooltip>
-        </ListItem>
-        <ListItem disablePadding sx={{ display: "block" }}>
-          <Tooltip title="Sent Items" placement="right">
-            <ListItemButton>...</ListItemButton>
-          </Tooltip>
-        </ListItem>
-        <ListItem disablePadding sx={{ display: "block" }}>
-          <Tooltip title="Common Bin" placement="right">
-            <ListItemButton>...</ListItemButton>
-          </Tooltip>
-        </ListItem>
-        <ListItem disablePadding sx={{ display: "block" }}>
-          <Tooltip title="Linked Queue" placement="right">
-            <ListItemButton>...</ListItemButton>
-          </Tooltip>
-        </ListItem>
-        <ListItem disablePadding sx={{ display: "block" }}>
-          <Tooltip title={"Queues Count:" + (queues?.length || 0)} placement="right">
-            <ListItemButton>...</ListItemButton>
-          </Tooltip>
-        </ListItem>
 
-        {/* Search Input Field */}
-        <ListItem disablePadding sx={{ display: "block" }}>
-          <TextField
-            variant="outlined"
-            placeholder="Search Queues"
-            value={searchInput}
-            onChange={handleSearchChange}
-            fullWidth
-          />
-        </ListItem>
+
+              
 
         <Collapse in={queuesOpen} timeout="auto" unmountOnExit>
           <Divider />
