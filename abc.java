83e1e149396a0d2cd2cfbdb59f061f6c335814d1
Vineeta Singh <65161314+vineeta23@users.noreply.github.com>
2024-07-12 12:33:08 +0530

    Update abc.java

--- abc.java
+++ abc.java
@@ -12,3 +12,27 @@
       searchFieldRef.current.focus();
     }
   }, []);
+
+
+
+
+  sx={{
+    height: "55vh",
+    marginRight: "15px",
+    overflowY: "auto",
+    overflowX: "hidden",
+    '&::-webkit-scrollbar': {
+      width: '8px', /* Width of the scrollbar */
+    },
+    '&::-webkit-scrollbar-track': {
+      background: '#1d1d1d', /* Track background color */
+    },
+    '&::-webkit-scrollbar-thumb': {
+      background: '#888', /* Scrollbar thumb color */
+      borderRadius: '10px', /* Thumb border radius */
+    },
+    '&::-webkit-scrollbar-thumb:hover': {
+      background: '#555', /* Scrollbar thumb color on hover */
+    },
+  }}
+>
