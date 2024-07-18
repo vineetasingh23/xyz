9c521748af22f28d9697d5ef08d6437c3e4c8e01
brb0023 <65161314+brb0023@users.noreply.github.com>
2024-07-18 12:19:19 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,59 +1,9 @@
-import React, { useRef, useEffect } from 'react';
-import { TextField, InputAdornment } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-
-interface SearchInputProps {
-  searchInput: string;
-  handleSearchChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-}
-
-const SearchInput: React.FC<SearchInputProps> = ({ searchInput, handleSearchChange }) => {
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    if (inputRef.current) {
-      inputRef.current.focus();
-    }
-  }, [searchInput]);
-
-  return (
-    <TextField
-      inputRef={inputRef}
-      variant="outlined"
-      placeholder="Search Queues"
-      value={searchInput}
-      onChange={handleSearchChange}
-      fullWidth
-      InputProps={{
-        startAdornment: (
-          <InputAdornment position="start">
-            <SearchIcon style={{ color: 'white', height: '20px' }} />
-          </InputAdornment>
-        ),
-        sx: {
-          color: 'white',
-          '& .MuiOutlinedInput-input': {
-            color: 'white',
-          },
-          '& .MuiInputAdornment-root svg': {
-            color: 'white',
-          },
-          '& .MuiOutlinedInput-notchedOutline': {
-            borderColor: 'white',
-          },
-          '&:hover .MuiOutlinedInput-notchedOutline': {
-            borderColor: 'white',
-          },
-          '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-            borderColor: 'white',
-          },
-        }
-      }}
-      sx={{
-        marginBottom: '10px',
-      }}
-    />
-  );
-};
-
-export default SearchInput;
+{open && queuesOpen && queues?.length > 10 && (
+              <ListItem disablePadding sx={{ display: 'flex', justifyContent: 'center' }}>
+                <Box sx={{ width: '80%', maxWidth: 400 }}>
+                  <SearchInput searchInput={searchInput} handleSearchChange={handleSearchChange} />
+                </Box>
+              </ListItem>
+            )}
+          </ListItem>
+        </List>
