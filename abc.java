a7662ab036bb17152a16a8bae7a79772d1fc13f7
bobthebuilder2341 <65161314+bobthebuilder2341@users.noreply.github.com>
2024-09-02 11:21:36 +0530

    Update abc.java

--- abc.java
+++ abc.java
@@ -1,23 +1,15 @@
-import { styled, TextField } from '@mui/material';
-import React from 'react';
-import { lightTheme, darkTheme } from '/theme';
-
-export const StyledTextField = styled(TextField)(({ theme }) => ({
-  '& .MuiOutlinedInput-root': {
-    width: '310px',
-    borderRadius: '10px',
-    justifyContent: 'center',
-    color: theme.palette.mode === 'light' ? lightTheme.fontColor : darkTheme.fontColor, // Ensuring text color is visible
-    '& fieldset': {
-      border: `1px solid`,
-      borderColor: theme.palette.mode === 'light' ? lightTheme.primaryColor : darkTheme.primaryColor,
-      backgroundColor: theme.palette.mode === 'light' ? lightTheme.white : 'transparent',
-    },
-  },
-  '& .MuiInputLabel-root': {
-    color: theme.palette.mode === 'light' ? lightTheme.fontColor : darkTheme.white, // Ensuring label color is visible
-  },
-  '& .MuiOutlinedInput-input': {
-    color: theme.palette.mode === 'light' ? 'black' : 'white', // Text color based on theme mode
-  },
-}));
+<StyledTextField
+            value={formState[item.fieldName] || ""}
+            onChange={(e) => handleFieldChange(item.fieldName, e.target.value)}
+            label={item.fieldName}
+            defaultValue=""
+            size="small"
+            sx={{
+              '& label': {
+                color: theme.palette.mode === 'light' ? 'black' : 'white',
+              },
+              '& .MuiInputBase-input': {
+                color: theme.palette.mode === 'light' ? 'black' : 'white',
+              },
+            }}
+          />
