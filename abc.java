abdb9251a57f31c639be2e9ea121efbf978ba2d0
bobthebuilder2341 <65161314+bobthebuilder2341@users.noreply.github.com>
2024-09-05 00:44:18 +0530

    Update abc.java

--- abc.java
+++ abc.java
@@ -1,45 +1,26 @@
-case "dropdown":
-  return (
-    <FormControl fullWidth>
-      <InputLabel
-        id={`${item.fieldName}-input-select-label`}
-        sx={{
-          color:
-            useTheme().palette.mode === "light"
-              ? lightTheme.black
-              : darkTheme.white + " !important",
-        }}
-      >
-        {item.fieldName}
-      </InputLabel>
-      <StyledSelect
-        labelId={`${item.fieldName}-input-select-label`}
-        id={`${item.fieldName}-select`}
-        value={formState[item.fieldName] || "select"}
-        label={item.fieldName}
-        onChange={(e) => handleFieldChange(item.fieldName, e.target.value)}
-        sx={{ height: "40px" }}
-        disabled={isDisabled}
-        MenuProps={{
-          PaperProps: {
-            sx: {
-              "& .MuiMenuItem-root.Mui-disabled": {
-                color: "red", // Color for the disabled item
-              },
-            },
-          },
-        }}
-        IconComponent={isDisabled ? NotInterestedIcon : undefined} 
-      >
-        <MenuItem value="select" disabled>
-          <em>Select</em>
-        </MenuItem>
-        {item.options &&
-          item.options.map((option: string, index: number) => (
-            <MenuItem key={index} value={option}>
-              {option}
-            </MenuItem>
-          ))}
-      </StyledSelect>
-    </FormControl>
-  );
+const handleSave = async () => {
+  const payload = processForm.items.reduce((acc, item) => {
+    acc[item.emailMgmtMapping] = formState[item.fieldName];
+    return acc;
+  }, {});
+
+  try {
+    const url = `${ApiEndPoints.ROOT}${ApiEndPoints.REQUEST}/${ApiEndPoints.PROCESS_FORM}`;
+    const axiosConfig: AxiosRequestConfig = {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+        "ct-remote-user": userDetails.emailId,
+      },
+    };
+
+    const response: AxiosResponse = await ApiConfig.put(url, payload, axiosConfig);
+    setSnackbarMessage("Process form saved successfully!");
+    setSnackbarSeverity("success");
+    setSnackbarOpen(true);
+  } catch (error) {
+    setSnackbarMessage("Error saving Process Forms");
+    setSnackbarSeverity("error");
+    setSnackbarOpen(true);
+  }
+};
