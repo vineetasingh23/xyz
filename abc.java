ad412a4031408bb894b966920a3ebbfbe42d43ea
bobthebuilder2341 <65161314+bobthebuilder2341@users.noreply.github.com>
2024-11-29 17:29:38 +0530

    Update abc.java

--- abc.java
+++ abc.java
@@ -1,16 +1,23 @@
-const [disableAutoSave, setDisableAutoSave] = useState(false);
+import React from "react";
+import { Checkbox } from "@mui/material";
+import { styled, alpha } from "@mui/material/styles";
+import { darkTheme, lightTheme } from "../Constants/theme";
 
-const handleButtonClick = () => {
-  setDisableAutoSave(true);
-  // Perform button-specific actions
-  setTimeout(() => setDisableAutoSave(false), 5000); // Resume autosave after 5 seconds
-};
-
-useEffect(() => {
-  if (!disableAutoSave) {
-    const interval = setInterval(() => {
-      saveDraft();
-    }, 5000);
-    return () => clearInterval(interval);
-  }
-}, [disableAutoSave]);
+export const CustomCheckbox = styled(Checkbox)(({ theme }) => ({
+  color: theme.palette.mode === "light" ? lightTheme.darkBlue : darkTheme.darkBlue,
+  "&.Mui-checked": {
+    color: theme.palette.mode === "light" ? lightTheme.darkBlue : darkTheme.darkBlue,
+  },
+  "&:hover": {
+    backgroundColor: alpha(
+      theme.palette.mode === "light" ? lightTheme.iconBlue : darkTheme.iconBlue,
+      theme.palette.action.hoverOpacity
+    ),
+  },
+  "&.Mui-disabled": {
+    color: theme.palette.mode === "light" ? theme.palette.grey[400] : theme.palette.grey[600],
+  },
+  "&.Mui-disabled + .MuiCheckbox-track": {
+    opacity: theme.palette.mode === "light" ? 0.6 : 0.3,
+  },
+}));
